8dea14931e2d62b86fb887c65d83b7db59640e7a
Villu Ruusmann <villu.ruusmann@gmail.com>
2015-08-28 22:45:50 +0300

    Improved code coverage

--- openscoring-service/src/test/java/org/openscoring/service/ModelResourceTest.java
+++ openscoring-service/src/test/java/org/openscoring/service/ModelResourceTest.java
@@ -22,6 +22,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URI;
 import java.util.List;
 
 import javax.ws.rs.client.Entity;
@@ -31,6 +32,9 @@ import javax.ws.rs.core.Response;
 
 import org.dmg.pmml.FieldName;
 import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 import org.openscoring.common.BatchEvaluationRequest;
@@ -53,6 +57,8 @@ public class ModelResourceTest extends JerseyTest {
 
 	@Override
 	protected void configureClient(ClientConfig clientConfig){
+		clientConfig.register(MultiPartFeature.class);
+
 		// Ideally, should use the client-side ObjectMapperProvider class instead of the server-side one
 		clientConfig.register(ObjectMapperProvider.class);
 	}
@@ -61,9 +67,13 @@ public class ModelResourceTest extends JerseyTest {
 	public void decisionTreeIris() throws Exception {
 		String id = "DecisionTreeIris";
 
+		assertEquals("Iris", extractSuffix(id));
+
 		deploy(id);
 
-		List<EvaluationRequest> records = loadRecords("Iris");
+		download(id);
+
+		List<EvaluationRequest> records = loadRecords(id);
 
 		EvaluationRequest request = records.get(0);
 
@@ -76,9 +86,11 @@ public class ModelResourceTest extends JerseyTest {
 	public void associationRulesShopping() throws Exception {
 		String id = "AssociationRulesShopping";
 
-		deploy(id);
+		assertEquals("Shopping", extractSuffix(id));
 
-		List<EvaluationRequest> records = loadRecords("Shopping");
+		deployForm(id);
+
+		List<EvaluationRequest> records = loadRecords(id);
 
 		BatchEvaluationRequest request = new BatchEvaluationRequest();
 		request.setRequests(records);
@@ -94,7 +106,11 @@ public class ModelResourceTest extends JerseyTest {
 
 		assertEquals(request.getId(), response.getId());
 
-		undeploy(id);
+		evaluateCsv(id);
+
+		evaluateCsvForm(id);
+
+		undeployForm(id);
 	}
 
 	private ModelResponse deploy(String id) throws IOException {
@@ -113,6 +129,43 @@ public class ModelResourceTest extends JerseyTest {
 		assertEquals(201, response.getStatus());
 
 		return response.readEntity(ModelResponse.class);
+	}
+
+	private ModelResponse deployForm(String id) throws IOException {
+		Response response;
+
+		InputStream is = openPMML(id);
+
+		try {
+			FormDataMultiPart formData = new FormDataMultiPart();
+			formData.field("id", id);
+			formData.bodyPart(new FormDataBodyPart("pmml", is, MediaType.APPLICATION_XML_TYPE));
+
+			Entity<FormDataMultiPart> entity = Entity.entity(formData, MediaType.MULTIPART_FORM_DATA);
+
+			response = target("model").request(MediaType.APPLICATION_JSON).post(entity);
+
+			formData.close();
+		} finally {
+			is.close();
+		}
+
+		assertEquals(201, response.getStatus());
+
+		URI location = response.getLocation();
+
+		assertEquals("/model/" + id, location.getPath());
+
+		return response.readEntity(ModelResponse.class);
+	}
+
+	private Response download(String id){
+		Response response = target("model/" + id + "/pmml").request(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML).get();
+
+		assertEquals(200, response.getStatus());
+		assertEquals(MediaType.APPLICATION_XML_TYPE.withCharset(CHARSET_UTF_8), response.getMediaType());
+
+		return response;
 	}
 
 	private EvaluationResponse evaluate(String id, EvaluationRequest request){
@@ -135,10 +188,65 @@ public class ModelResourceTest extends JerseyTest {
 		return response.readEntity(BatchEvaluationResponse.class);
 	}
 
-	private void undeploy(String id){
+	private Response evaluateCsv(String id) throws IOException {
+		Response response;
+
+		InputStream is = openCSV(id);
+
+		try {
+			Entity<InputStream> entity = Entity.entity(is, MediaType.TEXT_PLAIN_TYPE.withCharset(CHARSET_ISO_8859_1));
+
+			response = target("model/" + id + "/csv").request(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN).post(entity);
+		} finally {
+			is.close();
+		}
+
+		assertEquals(200, response.getStatus());
+		assertEquals(MediaType.TEXT_PLAIN_TYPE.withCharset(CHARSET_ISO_8859_1), response.getMediaType());
+
+		return response;
+	}
+
+	private Response evaluateCsvForm(String id) throws IOException {
+		Response response;
+
+		InputStream is = openCSV(id);
+
+		try {
+			FormDataMultiPart formData = new FormDataMultiPart();
+			formData.bodyPart(new FormDataBodyPart("csv", is, MediaType.TEXT_PLAIN_TYPE));
+
+			Entity<FormDataMultiPart> entity = Entity.entity(formData, MediaType.MULTIPART_FORM_DATA);
+
+			response = target("model/" + id + "/csv").request(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN).post(entity);
+
+			formData.close();
+		} finally {
+			is.close();
+		}
+
+		assertEquals(200, response.getStatus());
+		assertEquals(MediaType.TEXT_PLAIN_TYPE.withCharset(CHARSET_UTF_8), response.getMediaType());
+
+		return response;
+	}
+
+	private Response undeploy(String id){
 		Response response = target("model/" + id).request(MediaType.APPLICATION_JSON).delete();
 
 		assertEquals(204, response.getStatus());
+		assertEquals(null, response.getMediaType());
+
+		return response;
+	}
+
+	private Response undeployForm(String id){
+		Response response = target("model/" + id).request(MediaType.APPLICATION_JSON).header("X-HTTP-Method-Override", "DELETE").post(null);
+
+		assertEquals(204, response.getStatus());
+		assertEquals(null, response.getMediaType());
+
+		return response;
 	}
 
 	static
@@ -169,6 +277,23 @@ public class ModelResourceTest extends JerseyTest {
 
 	static
 	private InputStream openCSV(String id){
-		return ModelResourceTest.class.getResourceAsStream("/csv/" + id + ".csv");
+		return ModelResourceTest.class.getResourceAsStream("/csv/" + extractSuffix(id) + ".csv");
 	}
+
+	static
+	private String extractSuffix(String id){
+
+		for(int i = id.length() - 1; i > -1; i--){
+			char c = id.charAt(i);
+
+			if(Character.isUpperCase(c)){
+				return id.substring(i);
+			}
+		}
+
+		throw new IllegalArgumentException();
+	}
+
+	private static final String CHARSET_UTF_8 = "UTF-8";
+	private static final String CHARSET_ISO_8859_1 = "ISO-8859-1";
 }
